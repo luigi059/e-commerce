bd1bb58985e296352463b754f1426fadfdbfcaa7
Luigi Fernandez <luigimarkfernandez@gmail.com>
2021-01-11 01:09:20 +0000

    Login Register + Accessing Tokens (client)

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,10 +1,20 @@
-import React,{createContext,useState} from 'react';
+import axios from 'axios';
+import React,{createContext,useState,useEffect} from 'react';
 import ProductsAPI from "./api/ProductsAPI";
 
 export const GlobalState = createContext();
 
 export const DataProvider = ({children}) => {
     const [token,setToken] = useState(false);
+
+    const refreshToken = async()=>{
+        const res = await axios.get("/user/refresh_token");
+        setToken(res.data.accessToken);
+    }
+    useEffect(()=>{
+        const firstLogin = localStorage.getItem("firstLogin");
+        if(firstLogin) refreshToken();
+    },[])
 
     const state={
         token:[token,setToken],
--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,18 +1,22 @@
 import React,{useContext} from 'react';
 import {GlobalState} from "../../GlobalState";
 import ProductItem from "../utils/product_item/ProductItem";
+import Loading from "../utils/loading/Loading";
 
 export default function Product() {
     const state = useContext(GlobalState);
     console.log(state.ProductsAPI.products)
     const [products] = state.ProductsAPI.products;
     return (
-        <div className="products">
-            {
-                products.map(product => {
-                    return <ProductItem key={product._id} product={product}/>
-                })
-            }
-        </div>
+        <>
+            <div className="products">
+                {
+                    products.map(product => {
+                        return <ProductItem key={product._id} product={product}/>
+                    })
+                }
+            </div>
+            {products.length === 0 && <Loading/>}
+        </>
     )
 }
